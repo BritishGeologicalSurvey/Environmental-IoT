f179443c1d784823c83fea974867d9f5015b0121
Christopher Johnson <cjohn@ceh.ac.uk>
2016-02-03 17:11:52 +0000

    Adding script to load in the mongodb archive

--- shell/load_data.sh
+++ shell/load_data.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # Populate the local mongo database with the data
-
-mongoimport -d mydb -c everything --type csv --file /vagrant/data/All\ data\ 23-11-15\ to\ 21-12-15.csv --headerline
+cd /vagrant/data
+mkdir envdata
+tar -xvf /vagrant/data/envdata.tar.gz -C envdata
+mongorestore envdata
