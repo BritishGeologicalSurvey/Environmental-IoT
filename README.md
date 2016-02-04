09ea087bca9f987a259f09337840513318d1f9bb
Christopher Johnson <cjohn@ceh.ac.uk>
2016-02-04 13:39:10 +0000

    Adding .gitattributes for line endings

--- README.md
+++
@@ -1,4 +0,0 @@
-# Environmental-IoT
-
-Understanding and Managing the Natural Environment through Internet of Things Technology
-
--- shell/load_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Populate the local mongo database with the data
-cd /vagrant/data
-mkdir envdata
-tar -xvf /vagrant/data/envdata.tar.gz -C envdata
-mongorestore envdata
-
-# Copy over the map-locations.js
-cp map-locations.js /vagrant/code/public/js/components/.
